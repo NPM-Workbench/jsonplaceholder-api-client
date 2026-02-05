a480672c32572ff98b3ffb01e161b56af190583c
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-02-05 16:56:42 +0530

    [feat] Photos
    - update photos by id
      1. created a function that helps to update a photo
      2. exporting function as well as the required response types
      3. updated root barrel export and types file
    
    Note: resource may or may not be created by the API service provide.
    Checkout details at https://jsonplaceholder.typicode.com/

---
+++ src/photos/update-photo-by-id.ts
@@ -0,0 +1,59 @@
+/* node modules */
+import { TAPIResponse, TPhoto } from "../types/index.js";
+import { API_BASE_URL } from "../shared/api-base-url.js";
+
+/* types */
+type TInput = {
+  id: number,
+  data: { id: number, albumId: number, title: string, url: string, thumbnailUrl: string }
+};
+type TUpdatedPhotoRes = TAPIResponse & {
+  payload: null | {
+    updated: boolean,
+    photo: TPhoto | null
+  }
+};
+
+/* module */
+async function updatePhotoById(props: TInput): Promise<TUpdatedPhotoRes> {
+  try {
+    if (typeof fetch !== "function") {
+      throw new Error("[Bad]: Global Fetch Not Available");
+    } else {
+      /* setup and fetch */
+      const { id } = props;
+      const API_URL = `${API_BASE_URL}/photos/${id}`;
+      const response = await fetch(API_URL, {
+        method: "PUT",
+        body: JSON.stringify(props.data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+
+      /* check and return */
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+        const photo = await response.json();
+        const payload = Object.keys(photo).length <= 0 ? {updated: false, photo: null} : {updated: true, photo};
+        return {
+          code: "api-ok",
+          message: "No errors encountered. Check payload.",
+          payload
+        };
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      code: "api-fail",
+      message: "[Bad]: Update Photo By Id. Encountered Error.",
+      payload: { updated: false, photo: null }
+    }
+  }
+}
+
+/* exports */
+export type { TUpdatedPhotoRes };
+export { updatePhotoById };
